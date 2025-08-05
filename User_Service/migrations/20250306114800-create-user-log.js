9427626a1b66fa2a33d3046d035d283aef58bd05
Raja-Saqib <rajamsaqib1@gmail.com>
2025-08-05 17:56:35 +0500

    EB-1, EB-5, EB-9, EB-13: Implemented User Service Database, Express.JS App, Integration of Database with App, and APIs.

---
+++ User_Service/migrations/20250306114800-create-user-log.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('User_Logs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      action: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      timestamp: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('User_Logs');
+  }
+};
